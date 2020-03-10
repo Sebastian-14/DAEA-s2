2af8e6fc17043d78cd4b362e503cb1c40ff7ac0c
Sebastian <sebas14121999@gmail.com>
2020-03-09 21:57:14 -0500

    first commit

--- Semana2/CasoPropuesto02.Designer.cs
+++ Semana2/CasoPropuesto02.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.DgListaPedidos = new System.Windows.Forms.DataGridView();
+            this.DgListaProductos = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DgListaPedidos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgListaProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgListaPedidos
+            // 
+            this.DgListaPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgListaPedidos.Location = new System.Drawing.Point(44, 95);
+            this.DgListaPedidos.Name = "DgListaPedidos";
+            this.DgListaPedidos.RowHeadersWidth = 51;
+            this.DgListaPedidos.RowTemplate.Height = 24;
+            this.DgListaPedidos.Size = new System.Drawing.Size(930, 245);
+            this.DgListaPedidos.TabIndex = 0;
+            this.DgListaPedidos.DoubleClick += new System.EventHandler(this.DgListaPedidos_DoubleClick);
+            // 
+            // DgListaProductos
+            // 
+            this.DgListaProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgListaProductos.Location = new System.Drawing.Point(44, 410);
+            this.DgListaProductos.Name = "DgListaProductos";
+            this.DgListaProductos.RowHeadersWidth = 51;
+            this.DgListaProductos.RowTemplate.Height = 24;
+            this.DgListaProductos.Size = new System.Drawing.Size(930, 159);
+            this.DgListaProductos.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pedidos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 369);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Productos";
+            // 
+            // CasoPropuesto02
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1025, 656);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DgListaProductos);
+            this.Controls.Add(this.DgListaPedidos);
+            this.Name = "CasoPropuesto02";
             this.Text = "CasoPropuesto02";
+            this.Load += new System.EventHandler(this.CasoPropuesto02_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgListaPedidos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgListaProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView DgListaPedidos;
+        private System.Windows.Forms.DataGridView DgListaProductos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Semana2/CasoPropuesto02.cs
+++ Semana2/CasoPropuesto02.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Semana2
 {
@@ -15,6 +17,50 @@ namespace Semana2
         public CasoPropuesto02()
         {
             InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["Sebas"].ConnectionString);
+
+
+        public void ListaPedidos()
+        {
+            Console.WriteLine("iniciando");
+            using (SqlDataAdapter Df = new SqlDataAdapter("Select * from Pedidos", cn))
+            {
+                using (DataSet Da = new DataSet())
+                {
+                    // Llenar el DataSet mediante el método FILL del SqlAdapter
+                    Df.Fill(Da, "Pedidos");
+                    // Mostrar los datos en el control DataGridView
+                    DgListaPedidos.DataSource = Da.Tables["Pedidos"];
+                }
+            }
+        }
+
+        private void CasoPropuesto02_Load(object sender, EventArgs e)
+        {
+            ListaPedidos();
+        }
+
+        private void DgListaPedidos_DoubleClick(object sender, EventArgs e)
+        {
+            int Codigo;
+            Codigo = Convert.ToInt32(DgListaPedidos.CurrentRow.Cells[0].Value);
+            using (SqlCommand cmd = new SqlCommand("Usp_Detalle_Pedido", cn))
+            {
+                using (SqlDataAdapter Da = new SqlDataAdapter())
+                {
+                    Da.SelectCommand = cmd;
+                    Da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    Da.SelectCommand.Parameters.AddWithValue("@idpedido", Codigo);
+                    using (DataSet df = new DataSet())
+                    {
+                        Da.Fill(df, "detallespedidos");
+                        //mostrar los datos en el datagriedview
+                        DgListaProductos.DataSource = df.Tables["detallespedidos"];
+                    }
+                }
+            }
         }
     }
 }
